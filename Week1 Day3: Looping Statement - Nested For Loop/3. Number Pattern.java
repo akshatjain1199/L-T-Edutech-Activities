81c6cccb6216d3fcc1701d2d05cb1a2000d0d2f6
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-02-28 19:40:43 +0530

    Create 3. Number Pattern.java

---
+++ Week1 Day3: Looping Statement - Nested For Loop/3. Number Pattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
