366d55d75e936739c4b9b7852646f8a733d986a7
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-02-28 19:27:37 +0530

    Create Divisible by 2 or 3.java

---
+++ Week1 Day2: Conditional Statements/Divisible by 2 or 3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    Scanner scn= new Scanner(System.in);
+    int n= scn.nextInt();
+    if(n%2==0 || n%3==0)
+    {
+        System.out.println("yes");
+    }
+    else
+    {
+        System.out.println("no");
+    }
+     }
+}
