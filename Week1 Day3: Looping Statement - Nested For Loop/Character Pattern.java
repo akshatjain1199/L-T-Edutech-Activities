a013bb6dac174b15b619abb66dd501b6bc8efdb0
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-02-28 19:41:14 +0530

    Create Character Pattern.java

---
+++ Week1 Day3: Looping Statement - Nested For Loop/Character Pattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    Scanner scn= new Scanner(System.in);
+    int n= scn.nextInt();
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+        
+     }
+}
