5566e97de60062141c56da9924dcd6abaa79b8c1
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-02-28 19:34:58 +0530

    Create 1. Print all the numbers between two numbers.java

---
+++ Week1 Day3: Looping Statement - For Loop/1. Print all the numbers between two numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    System.out.println("Enter the value of a");
+    Scanner scn= new Scanner(System.in);
+    int a= scn.nextInt();
+    System.out.println("Enter the value of b");
+    int b= scn.nextInt();
+    for(int i=a;i<=b;i++)
+    {
+        System.out.println(i);
+    }
+     }
+}
