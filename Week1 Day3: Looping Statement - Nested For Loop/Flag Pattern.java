300d2ee0a66d795926dffbbe26bb830511b270a6
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-02-28 19:43:31 +0530

    Create Flag Pattern.java

---
+++ Week1 Day3: Looping Statement - Nested For Loop/Flag Pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    Scanner scn= new Scanner(System.in);    
+    int n= scn.nextInt();
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i == n / 2 + 1 && j == n / 2 + 1){
+                System.out.print("*");
+            }
+            else
+            {
+                System.out.print("~");
+            }
+        }
+        System.out.println();
+    }
+        
+     }
+}
