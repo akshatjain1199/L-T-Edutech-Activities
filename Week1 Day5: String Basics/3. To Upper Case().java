980f8a421477dac444bd23a1e4300932fad73072
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-03-01 01:15:42 +0530

    Create 3. To Upper Case().java

---
+++ Week1 Day5: String Basics/3. To Upper Case().java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+	public static void main(String []args){
+	Scanner scn= new Scanner(System.in);
+	System.out.println("Enter the name:");
+	String s= scn.nextLine();
+	System.out.println("Name:"+s.toUpperCase());
+	}
+}
