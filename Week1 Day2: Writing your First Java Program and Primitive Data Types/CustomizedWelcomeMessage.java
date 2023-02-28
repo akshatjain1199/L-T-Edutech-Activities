5b1b53b1f6b637e669f568846284f4fe4442fc2a
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-02-28 19:12:43 +0530

    Create CustomizedWelcomeMessage.java

---
+++ Week1 Day2: Writing your First Java Program and Primitive Data Types/CustomizedWelcomeMessage.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+	System.out.println("Enter your name");
+	Scanner scn = new Scanner(System.in);
+	String str = scn.nextLine();
+		System.out.println("Hello " + str + "! Welcome to Event Management System.");
+	
+	}
+
+}
