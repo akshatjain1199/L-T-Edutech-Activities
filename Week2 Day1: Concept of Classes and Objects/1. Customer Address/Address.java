417e42794155727894bfa4332ffed55299119de0
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-03-02 23:06:43 +0530

    Create Address.java

---
+++ Week2 Day1: Concept of Classes and Objects/1. Customer Address/Address.java
@@ -0,0 +1,14 @@
+ class Address {
+    String street;
+    String city;
+    int pincode;
+    String country;
+    
+    
+public  void displayAddress() {
+        System.out.println("Street: " + street);
+        System.out.println("City: " + city);
+        System.out.println("Pincode: " + pincode);
+        System.out.println("Country: " + country);
+    }
+}
