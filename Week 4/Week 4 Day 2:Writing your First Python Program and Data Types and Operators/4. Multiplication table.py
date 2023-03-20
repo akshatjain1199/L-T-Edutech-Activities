15ef7ab80335ce5458a925c1cfb39261623e78e8
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-03-20 21:15:29 +0530

    Create 4. Multiplication table.py

---
+++ Week 4/Week 4 Day 2:Writing your First Python Program and Data Types and Operators/4. Multiplication table.py
@@ -0,0 +1,10 @@
+
+
+if __name__=="__main__":
+    num= int(input())
+    n=int(input())
+    m=int(input())
+    for i in range(n,m+1):
+        result=num*n
+        print(f"{num} * {n} = {result}")
+        n=n+1
