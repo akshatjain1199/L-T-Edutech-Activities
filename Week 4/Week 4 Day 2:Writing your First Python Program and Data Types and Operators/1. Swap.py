0b2224469153df897a32cc8a8adf2ce53f2f258e
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-03-20 21:13:42 +0530

    Create 1. Swap.py

---
+++ Week 4/Week 4 Day 2:Writing your First Python Program and Data Types and Operators/1. Swap.py
@@ -0,0 +1,11 @@
+
+
+if __name__=="__main__":
+    num1= int(input())
+    num2= int(input())
+    print("Before Swapping the numbers : ",num1, num2)
+    print("After Swapping the numbers : ",num2,num1)
+    # num1=num1^num2
+    # num2=num1^num2
+    # num1=num1^num2
+    # print("After Swapping the numbers : ",num1,num2)
