ab882fd5899473b2c88b26c74e034104132ecf32
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-03-29 02:53:41 +0530

    Create staff.java

---
+++ Week 4/Week 4 Day 5: Object Relationship and Polymorphism/Design/1. Aggregation/staff.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Staff {
+    private String staffName;
+    private String designation;
+
+    public Staff(String staffName, String designation) {
+        this.staffName = staffName;
+        this.designation = designation;
+    }
+
+    public String getStaffName() {
+        return staffName;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+}
