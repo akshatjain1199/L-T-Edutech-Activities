1c437a7c208ad7fdbe24b681bec9642e3591c2f3
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-04-07 12:53:50 +0530

    Create Customer.java

---
+++ Week 5/Week 5 Day 1: Introduction to List/Design/6. Customer Details - LinkedList/Customer.java
@@ -0,0 +1,35 @@
+   public class Customer {
+    private String name;
+    private String email;
+    private String phoneNo;
+
+    public Customer(String name, String email, String phoneNo) {
+        this.name = name;
+        this.email = email;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+}
