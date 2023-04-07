864d0fff75c6adbdedd34872434884aff7553074
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-04-07 13:20:00 +0530

    Create User.java

---
+++ Week 5/Week 5 Day 2:HashSet/Design/4. User Search/User.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class User implements Comparable<User> {
+    private String username;
+    private String bankname;
+
+    public User() {
+    }
+
+    public User(String username, String bankname) {
+        this.username = username;
+        this.bankname = bankname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBankname() {
+        return bankname;
+    }
+
+    public void setBankname(String bankname) {
+        this.bankname = bankname;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        User user = (User) obj;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
+    @Override
+    public int compareTo(User other) {
+        return this.username.compareTo(other.username);
+    }
+}
