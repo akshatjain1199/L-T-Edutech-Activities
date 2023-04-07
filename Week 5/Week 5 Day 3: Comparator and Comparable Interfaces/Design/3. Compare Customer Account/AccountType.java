dca2b927c4887d2de3468e48792af9ec3ce4531a
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-04-07 17:24:40 +0530

    Create AccountType.java

---
+++ Week 5/Week 5 Day 3: Comparator and Comparable Interfaces/Design/3. Compare Customer Account/AccountType.java
@@ -0,0 +1,13 @@
+public class AccountType {
+    String accountType;
+    AccountType(){}
+    AccountType(String accountType){
+        this.accountType=accountType;
+    }
+    public void setAccountType(String accountType){
+        this.accountType=accountType;
+    }
+    public String getAccountType(){
+        return this.accountType;
+    }
+}
