75dbac49351cdef40972aa1363041bbf5b622990
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-04-07 16:51:22 +0530

    Create shape.java

---
+++ Week 4/Week 4 Day 5: Object Relationship and Polymorphism/Design/5. Area of shape/shape.java
@@ -0,0 +1,26 @@
+public class Shape
+{
+	protected  Double area;
+    
+    public Shape()
+    {
+    }
+    public Shape(Double area)
+    {
+        this.area=area;
+    }
+    
+    public void setArea(Double area)
+    {
+        this.area=area;
+    }
+    public Double getArea()
+    {
+        return this.area;
+    }
+    
+    public void computeArea()
+    {
+       // fill the code
+    }
+}
