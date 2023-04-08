0f872ac94ea524e35d83323865af4563a57ff14c
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-04-08 13:51:19 +0530

    Create Eventtype.java

---
+++ Week 6/Week6 Day 5: Exception Handling/Project/7. EventTypeDoesNotExistsException/Eventtype.java
@@ -0,0 +1,28 @@
+public class EventType{
+    private Long id;
+    private String name;
+
+    public EventType() {
+    }
+
+    public EventType(String name, Long id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
