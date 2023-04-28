5e6d5608561faa155f9000272f59abd9f00a61ee
akshatjain1199 <57901311+akshatjain1199@users.noreply.github.com>
2023-04-28 12:59:01 +0530

    Create script.js

---
+++ Week 8/Week8-Day5 :DOM Event Listeners/4. Hovering Event/script.js
@@ -0,0 +1,32 @@
+//fill your code
+function hoverRow1() {
+    document.getElementById("row1").innerHTML = "Acc Number: 8456844568845<br>Balance: 1223.46";
+}
+
+function removeHover1() {
+    document.getElementById("row1").innerHTML = "John";
+}
+
+function hoverRow2() {
+    document.getElementById("row2").innerHTML = "Acc Number: 8453333568845<br>Balance: 223.46";
+}
+
+function removeHover2() {
+    document.getElementById("row2").innerHTML = "William";
+}
+
+function hoverRow3() {
+    document.getElementById("row3").innerHTML = "Acc Number: 8456882214345<br>Balance: 123.46";
+}
+
+function removeHover3() {
+    document.getElementById("row3").innerHTML = "James";
+}
+
+function hoverRow4() {
+    document.getElementById("row4").innerHTML = "Acc Number: 4554589568845<br>Balance: 13.46";
+}
+
+function removeHover4() {
+    document.getElementById("row4").innerHTML = "Jack";
+}
